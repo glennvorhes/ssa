66d8ff17348cbc8aedaeafd02078aa2902a4f33f
Glenn Vorhes <gavorhes@wisc.edu>
2016-07-13 18:07:16 -0500

    use route id in map view

--- app/filters/filterBase.js
+++ app/filters/filterBase.js
@@ -6,4 +6,15 @@ import $ from 'webmapsjs/src/jquery/jquery';
 import provide from 'webmapsjs/src/util/provide';
 const nm = provide('ssa');
 
+class FilterBase{
+
+
+    constructor(topCheckId, otherChecksClass){
+
+    }
+
+}
+
+nm.FilterBase = FilterBase;
+export default FilterBase;
 
